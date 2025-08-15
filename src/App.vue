c16c1a03aefe6f37ed769d291391c53b032506e9
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-15 17:05:54 +0900

    #2 init LoginPage

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,29 @@
 <template>
-  <h2>{{ msg }}</h2>
-  <input type="text" v-model="testValue"/>
-  <button @click="alertMessage">나를 클릭해주세요.</button>
-  <PropsChildComponent :to-child="toChild"/>
-  <div>
-    <EmitChildComponent @click-child-button="alertEmitMessage(msg)"/>
+  <div class="container">
+    <LoginPage/>
   </div>
-  
 </template>
 
 <script>
-import PropsChildComponent from './components/PropsChildComponent.vue';
-import EmitChildComponent from './components/EmitChildComponent.vue';
+import LoginPage from './views/LoginPage.vue';
 
 export default {
   name: 'App',
-  components: {PropsChildComponent, EmitChildComponent },
-  data(){
-    return{
-      msg: "안녕하세요",
-      testValue: "",
-      toChild: "부모가 주는 데이터입니다.",
-    }
-  },
-  methods:{
-    alertMessage(){
-      alert('경고! 클릭했습니다.');
-    },
-    alertEmitMessage(msg){
-      alert(msg);
-    },
-  },
+  components: {LoginPage,},
 };
 </script>
 
 <style>
+body {
+  background-color: black;
+  color: white;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 </style>
