bff5fac42dee410e8b7d281e41b8cd6194dbcb8d
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-15 17:40:16 +0900

    #2 LoginPage Style

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <input :type="text" :placeholder="placeholder" :value="modelValue" @input="updateValue"/>
+  <div class="container">
+    <input class="input-form" :type="type" :placeholder="placeholder" :value="modelValue" @input="updateValue"/>
   </div>
 </template>
 
@@ -22,6 +22,20 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped> 
+.container{
+    height: fit-content;
+    width: 300px;
+}
+.input-form {
+  box-sizing: border-box; /* 패딩과 테두리를 포함한 너비 */
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid gray;
+  background-color: #000000;
+  margin-bottom: 10px;
+  font-size: 12px;
+  color: gray;
+}
 </style>
