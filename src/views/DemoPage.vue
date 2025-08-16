f7f6ba90f7ff6ca6af6ead85c63baf0114ff065b
zuyo7852 <leejuyeon7852@gmail.com>
2025-08-16 14:54:01 +0900

    #4 method

--- src/views/DemoPage.vue
+++ src/views/DemoPage.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
+   <div>
     <p>index: {{ index }}</p>
     <p>상품 가격: {{ price }}</p>
     <p>할인율: {{ discountRate }}</p>
-    <p>최종 가격: {{ finalPrice }}</p>
-  </div>
+    <p>computed로 계산한 최종 가격: {{ finalPrice }}</p>
+    <p>method로 계산한 최종 가격: {{ calculateFinalPrice() }}</p>
+    <div>
+      <button @click="updatePrice">updatePrice</button>
+    </div>
+    <div>
+      <button @click="updateDiscountRate">updateDiscountRate</button>
+    </div>
+    <div>
+      <button @click="updateIndex">updateIndex</button>
+    </div>
+ </div>
 </template>
 
 <script>
 export default {
     name: "DemoPage",
-    data(){
-      return{
+     data() {
+      return {
         index: 0,
         price: 10000,
         discountRate: 0.1
@@ -19,7 +29,22 @@ export default {
     },
     computed: {
       finalPrice() {
-        console.log("call finalPrice");
+        console.log("computed!!");
+        return this.price * (1 - this.discountRate);
+      }
+    },
+    methods: {
+      updatePrice(){
+        this.price = this.price+1;
+      },
+      updateDiscountRate(){
+        this.discountRate = this.discountRate+0.1;
+      },
+      updateIndex(){
+        this.index = this.index+1;
+      },
+      calculateFinalPrice() {
+        console.log("method")
         return this.price * (1 - this.discountRate);
       }
     }
